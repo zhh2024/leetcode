e3c89a8b745c532d2b31bba53bdcc8feb9f6793a
zhh的R9000 <15536508342@163.com>
2024-03-15 15:35:24 +0800

    新代码

--- src/linkedList/MergeTwoLists.java
+++ src/linkedList/MergeTwoLists.java
@@ -23,6 +23,16 @@ public class MergeTwoLists {
 
     }
 
+    /**
+     * 时间复杂度O(m+n) 空间复杂度O(1)
+     * 解题思路:
+     * 1. 使用数组双指针思想,链表与数组不一样的是,插入到原数组中，原数组后面的数据往后移动即可
+     * 2. 链表插入到原链表中,需要pre节点指向当前插入的节点,当前插入节点再指向原链表当前节点,需要pre指针
+     * 3. pre指针有可能是null,因为一开始就插入头节点呢？所以需要虚节点
+     * @param list1
+     * @param list2
+     * @return
+     */
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2){
         //选定list2作为被插入链表 插入规则list1小,插入到list2前面, list1指向list2,list1右移 ,所以需要list2的pre节点
         //但pre节点有可能为null,所以需要虚节点
@@ -33,24 +43,48 @@ public class MergeTwoLists {
         ListNode pre = dummyNode;
         while (list1!=null && list2!=null){
             if (list1.val > list2.val) {
-                pre = list2;
                 //list1大 ,list2右移
                 list2 = list2.next;
             }else {
                 //list1小,插入到list2前面, list1指向list2,list1右移 ,所以需要list2的pre节点
                 ListNode ln = list1.next;
                 pre.next = list1;
-                pre = list1;
                 list1.next = list2;
                 list1 = ln;
             }
+            //不管是否插入,每次遍历,pre都是要往前走一步的
+            pre = pre.next;
         }
         pre.next = list1 == null?list2:list1;
-        //第一种情况,list2为null,此时 ListNode pre = dummyNode;  pre.next = list1; 返回的就是list1头节点
-        //第二种情况，list2遍历结束,pre被重新赋值,返回的应该是list2头节点  dummyNode.next = list2;
-        //第三种情况，一开始list1>list2,应该返回list1头节点，ListNode pre = dummyNode; pre.next = list1;
-        //          此时dummyNode的next被赋值为list1头节点 后pre被赋值，dummyNode.next就是 list1头节点
-        //第四种情况，一开始list1<list2,返回list2头节点,    pre = list2;pre被赋值, dummyNode.next = list2;
         return dummyNode.next;
+    }
+
+    public static ListNode mergeTwoLists02(ListNode list1, ListNode list2){
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        if (list1.val > list2.val) {
+            list2.next = mergeTwoLists02(list1,list2.next);
+            return list2;
+        }else {
+            list1.next = mergeTwoLists02(list1.next,list2);
+            return list1;
+        }
+    }
+
+    /**
+     * 递归遍历链表
+     * @param node
+     * @return
+     */
+    public static ListNode digui(ListNode node){
+        if(node ==null){
+            return null;
+        }
+        node.next = digui(node.next);
+        return node;
     }
 }

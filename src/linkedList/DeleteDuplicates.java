4c26eee766633ba02c296bdc21fed03c18a276a3
zhh的R9000 <15536508342@163.com>
2024-04-03 17:04:40 +0800

    新代码

---
+++ src/linkedList/DeleteDuplicates.java
@@ -0,0 +1,56 @@
+package linkedList;
+
+/**
+ * @Desc: 删除排序链表中的重复元素 II
+ *
+ * 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+ * 示例 1：
+ * 输入：head = [1,2,3,3,4,4,5]
+ * 输出：[1,2,5]
+ *
+ * 示例 2：
+ * 输入：head = [1,1,1,2,3]
+ * 输出：[2,3]
+ * @Author：zhh
+ * @Date：2024/3/31 18:00
+ */
+public class DeleteDuplicates {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 时间复杂度O(n) 空间复杂度O(1)
+     * 解题思路: 双指针,定义 pre 和 curr两个指针
+     * 1. pre在head就被去重的情况下,pre是null,所以需要在head前定义一个虚节点,pre赋值虚节点,curr为head节点,双指针初始化完毕。
+     * 1. curr与 curr.next 比较 如果不一样,两个指针同时走
+     * 2. curr与 curr.next 比较 如果一样,需要curr开始往后遍历去重,直至遇到不一样的值,pre指向curr
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head){
+        ListNode dummyNode = new ListNode();
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode next = curr.next;
+            if(next == null){
+                break;
+            }
+            if(next.val != curr.val){
+                //下一个值与当前值不一样,往前走一步
+                curr = curr.next;
+                pre = pre.next;
+            }else {
+                //下一个值与当前值一样去重,找到不一样的新值
+                while (curr != null && curr.val == next.val){
+                    curr = curr.next;
+                }
+                //重新指向新值
+                pre.next = curr;
+            }
+        }
+        return dummyNode.next;
+    }
+}
--- src/linkedList/RemoveNthFromEnd.java
+++ src/linkedList/RemoveNthFromEnd.java
@@ -28,6 +28,7 @@ public class RemoveNthFromEnd {
     }
 
     /**
+     * 时间复杂度O(n)+O(n)
      * 解题思路:
      * 1. 遍历一次求出size
      * 2. 再遍历一次,size--,等size == n的时候，当前节点就是要删除的节点
@@ -60,6 +61,7 @@ public class RemoveNthFromEnd {
     }
 
     /**
+     * 时间复杂度O(n)+O(n) 空间复杂度O(n)
      * 解题思路: 利用栈的先进后出,倒数就变成了正数
      * 1. 先将node放入栈中,因为有可能第一个节点被删除(此时栈中就是null,会空指针异常)，所以需要虚节点也入栈
      * 2. 进行正序遍历,将n前面的都出栈,此时栈中first节点就是被删除节点的pre节点
@@ -85,6 +87,36 @@ public class RemoveNthFromEnd {
         //peek就不可能为null,如果是第一个节点被删除,peek此时就是虚节点。
         peek.next = peek.next.next;
         //如果是第一个节点被删除,peek此时就是虚节点。dummyNode.next 就是第二个节点，如果不是第一个节点被删除，dummyNode.next就是head
+        return dummyNode.next;
+    }
+
+    /**
+     * 时间复杂度O(n)
+     * 解题思路: 使用快慢指针,删除链表的倒数第 n 个结点,快指针 = 慢指针 + n ,这样快指针遍历结束，慢指针所指的下标就是被删除节点的前一个节点
+     * 1. 利用快指针 = 慢指针 + n,先遍历初始化快指针
+     * 2. 循环快指针,循环中慢指针和快指针一起走,直至快指针遍历结束
+     * 3. 此时最终慢指针的值就是删除节点的前一个节点
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd3(ListNode head, int n){
+        ListNode fastCurr = head;
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode lowCurr = dummyNode;
+        //先定位到fastCurr的位置
+        for (int i = 0; i < n; i++) {
+            fastCurr =fastCurr.next;
+        }
+        //然后快慢指针一起走
+        while (fastCurr!=null){
+            fastCurr = fastCurr.next;
+            lowCurr = lowCurr.next;
+        }
+        //此时lowCurr就是要删除的节点的前一个节点
+        lowCurr.next = lowCurr.next.next;
         return dummyNode.next;
     }
 }

76d7ae69e578a177add28b1059e12f4f62654d66
zhh2024 <15536508342@163.com>
2025-12-08 17:26:06 +0800

    复习

--- src/Zhh.java
+++ src/Zhh.java
@@ -1,3 +1,8 @@
+import linkedList.ListNode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * @Desc:
  * @Author：zhh
@@ -6,56 +11,10 @@
 public class Zhh {
 
     public static void main(String[] args) {
-        int[] matrix = {1,3};
-        search(matrix,3);
+
     }
 
-    public static  int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length -1;
-        int rightNum = nums[right];
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int midNum = nums[mid];
-            if(midNum == target){
-                return mid;
-            }
-            //1. 判断mid 在哪个区间？无非就两个有序区间
-            if(midNum <= rightNum){
-                //2. 确定在第二个区间,就可以判断取该区间的哪一边了
-                if(target < midNum || target > rightNum){
-                    right = mid - 1;
-                }else {
-                    left = mid + 1;
-                }
-            }else {
-                //2. 确定在第一个区间,就可以判断取该区间的哪一边了
-                if(target < midNum && target > rightNum){
-                    right = mid - 1;
-                }else {
-                    left = mid + 1;
-                }
-            }
 
-        }
-        return -1;
-    }
 
-    public static int findPeakElement02(int[] nums) {
-        int left = 0;
-        int right = nums.length -1;
 
-        //让left小于right，这样就不用考虑边界问题了。
-        while (left < right){
-            int mid = left + (right -left)/2;
-            int midNum = nums[mid];
-            //mid+1 不会越界的原因是，mid计算后逼近的是left。
-            if(midNum > nums[mid +1]){
-                right = mid ;
-            }else {
-                left = mid +1;
-            }
-        }
-        return left;
-    }
 }
--- src/tree/binarytree/traverse/BuildTree.java
+++ src/tree/binarytree/traverse/BuildTree.java
@@ -6,32 +6,40 @@ import java.util.HashMap;
 
 /**
  * @Desc: 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+ * 解题:
+ * 必须得有中序 + 前序 or 中序 + 后序。
+ * 前后没法判定一个二叉树，前序和后序都只能告诉我们谁是谁的根，但无法告诉我们哪些节点属于左子树，哪些属于右子树。
+ * 单中序遍历又没法确认根的位置。所以前或者后,必须得配合中序，来判断走哪边，判断条件就是是有该测是由有值。
  * @Author：zhh
  * @Date：2024/6/17 19:12
  */
 public class BuildTree {
 
     public static void main(String[] args) {
-        int[] preorder = {3,9,20,15,7};
-        int[] inorder = {9,3,15,20,7};
-        System.out.println(buildTree(preorder,inorder));
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        System.out.println(buildTree(preorder, inorder));
     }
+
+    //数组中的值必然是唯一的，如果不是唯一的，通过数组根本没法确认结构。所以可以利用哈希表，取中序数组的下标。
     static HashMap<Integer, Integer> map = new HashMap<>();
 
+    //必须定义在外层，因为递归传入进去的话，回溯时候，下标也会回溯回去，该场景，前序数组应该一直从前往后走，而不应该回溯回去。
     static int preorder_left = 0;
+
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
         int length = preorder.length;
 
         for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i],i);
+            map.put(inorder[i], i);
         }
-        return buildTree2(preorder,   0, length - 1);
+        return buildTree2(preorder, 0, length - 1);
     }
 
     public static TreeNode buildTree2(int[] preorder
-                                      ,int inorder_left,int inorder_right){
+            , int inorderLeft, int inorderRight) {
         //为根节点,左右子树为null
-        if(inorder_left > inorder_right){
+        if (inorderLeft > inorderRight) {
             return null;
         }
         //前序数组数组首位就是根,前序数组向前走
@@ -41,9 +49,9 @@ public class BuildTree {
         //通过根节点,拆分中序数组为左右子树
         Integer i = map.get(root);
         //递归指向左子树的根节点
-        rootNode.left = buildTree2(preorder,inorder_left,i-1);
+        rootNode.left = buildTree2(preorder, inorderLeft, i - 1);
         //递归指向右子树的根节点
-        rootNode.right = buildTree2(preorder,i+1,inorder_right);
+        rootNode.right = buildTree2(preorder, i + 1, inorderRight);
         return rootNode;
     }
 }
